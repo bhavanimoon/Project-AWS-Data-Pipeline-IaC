5a9f379611048c55947fed064fd13e3b7e639b7e
bhavaniMoon <bhavanisundar89@outlook.com>
2026-03-25 22:22:13 +0530

    Bucket creation configs updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,10 +4,18 @@ provider "aws" {
 
 resource "aws_s3_bucket" "test_bucket" {
   bucket = "bmoon-terraform-test-bucket"   # bucket name must be globally unique
-#  acl    = "private" # acl is deprecated. So adding another resource section for acl.
+  
+  # Enforce bucket owner ownership (disables ACLs)
+  object_ownership = "BucketOwnerEnforced"
+  
+  tags = {
+	Name = "bmoon-terraform-test-bucket"
+	Environment = "Dev"
+	}
 }
 
-resource "aws_s3_bucket_acl" "test_bucket_acl" {
-	bucket = aws_s3_bucket.test_bucket.id
-	acl = "private"
-}
+#  acl is deprecated. So commenting out the below section added for acl bucket creation.
+#resource "aws_s3_bucket_acl" "test_bucket_acl" {
+#	bucket = aws_s3_bucket.test_bucket.id
+#	acl = "private"
+#}
